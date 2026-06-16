194b602572f98aa24ac442e015f545f6ec1d8bc0
Carlos Eduardo Otávio Matias <dankaidh@gmail.com>
2026-06-16 12:01:26 -0300

    Improve output formatting in calculadora.py
    Refactor print statements for better formatting and clarity.

--- calculadora.py
+++ calculadora.py
@@ -1,25 +1,30 @@
-print ("Calculadora Simples")
+print("=== CALCULADORA ===")
 
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
 
-print("\n1 - Soma")
+print("\nEscolha uma operação:")
+print("1 - Soma")
 print("2 - Subtração")
 print("3 - Multiplicação")
-print("4 -  Divisão")
+print("4 - Divisão")
 
-opcao = input ("Escolha uma operação: ")
+opcao = input("\nOpção: ")
 
 if opcao == "1":
-   print("Resultado: ", n1 + n2)
+    print(f"\nResultado: {n1 + n2}")
+
 elif opcao == "2":
-   print("Resultado: ", n1 - n2)
+    print(f"\nResultado: {n1 - n2}")
+
 elif opcao == "3":
-   print("Resultado: ", n1 * n2)
+    print(f"\nResultado: {n1 * n2}")
+
 elif opcao == "4":
-   if n2 == 0:
-       print("Erro: Divisão por zero.")
-   else:
-       print("Resultado: ", n1 / n2)
+    if n2 == 0:
+        print("\nErro: divisão por zero não é permitida.")
+    else:
+        print(f"\nResultado: {n1 / n2}")
+
 else:
-    print("Opção inválida.")
+    print("\nOpção inválida.")
