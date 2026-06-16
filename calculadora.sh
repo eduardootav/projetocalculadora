a3df02a6adb25f483dac69d928ce5bfbae14f539
Carlos Eduardo Otávio Matias <dankaidh@gmail.com>
2026-06-16 11:55:20 -0300

    Replace shell calculator with Python script call
    Removed shell calculator implementation and added Python script execution.

--- calculadora.sh
+++ calculadora.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 
-echo "Projeto calculadora"
-
-read -p "Digite o primeiro número: " n1
-read -p "Digite o segundo número: " n2
-
-resultado=$((n1 + n2))
-
-echo "Resultado: $resultado"
-
-
+python3 calculadora.py
